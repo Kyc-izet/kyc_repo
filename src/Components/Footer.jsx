a555197ec6699f46807d72f5e2d0f772df14ba7b
mAyush24 <ayush2524mishra@gmail.com>
2023-10-29 20:58:44 +0530

    add footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -11,10 +11,8 @@ export default function footer() {
   return (
     <div>
       <footer className="footer">
-        {/* <div className="container_ flex_ spaced-between_">
-          <div className="image_ flex_">
-            <img src={image} />
-          </div>
+        <div className="container_ flex_ spaced-between_ aHide">
+          <img className='aImage' src={image} />
           <div className="middle_ flex_ col_">
             <div className="navbar_ flex_">
               <ul className="flex_ col_ left-align_">
@@ -34,23 +32,52 @@ export default function footer() {
               <hr />
             </div>
           </div>
-          <div className="links_ flex_ col_ left-align_">
+          <div className="links_ flex_ col_ left-align_ aHide">
             <p>GET OUR</p>
             <p>ASSISTANCES</p>
             <div className="social-icons flex">
-              
               <a href="https://www.linkedin.com/company/know-your-colleges/"><FaLinkedin fontSize={"2rem"} /></a>
               <a href="https://wa.me/message/37PSY2CRRSIJE1"><FaWhatsapp fontSize={"2rem"} /></a>
               <a href="https://instagram.com/knowyourcolleges_kyc?igshid=OGQ5ZDc2ODk2ZA=="><FaInstagram fontSize={"2rem"} /></a>
               <a href=""><FaEnvelope fontSize={"2rem"} /></a>
             </div>
           </div>
-        </div> */}
-        <div className="contact-info">
-          {/* <img src={image} alt="" /> */}
+        </div>
+
+        <div className="aShow">
+          <div className="aFoot">
+            <ul className="aList">
+              <li>ABOUT</li>
+              <li>TERMS OF USE</li>
+            </ul>
+            <ul className="aList">
+              <li>PRIVACY POLICY</li>
+              <li>CONTACT US</li>
+            </ul>
+            <ul className="aList">
+              <li>HELPS</li>
+              <li>FAQS</li>
+            </ul>
+          </div>
+          <hr style={{backgroundColor:'white', width:'80%',margin:'auto'}} />
+          <div className="assistance">
+            <p style={{ color: 'white' }}>GET OUR <span style={{ color: '#00ED64' }}>ASSISTANCES</span></p>
+            <div className=" alinks">
+              <a href="https://www.linkedin.com/company/know-your-colleges/"><FaLinkedin color='white' fontSize={"2rem"} /></a>
+              <a href="https://wa.me/message/37PSY2CRRSIJE1"><FaWhatsapp color='white' fontSize={"2rem"} /></a>
+              <a href="https://instagram.com/knowyourcolleges_kyc?igshid=OGQ5ZDc2ODk2ZA=="><FaInstagram color='white' fontSize={"2rem"} /></a>
+              <a href=""><FaEnvelope color='white' fontSize={"2rem"} /></a>
+            </div>
+          </div>
+          <div className="aImage">
+            <img className='' src={image} />
+            <p>Copyright ©2021-2022 knowyourcolleges.com-All rights reserved</p>
+          </div>
+        </div>
+        {/* <div className="contact-info">
           <p>Copyright ©2021-2022 knowyourcolleges.com-All rights reserved</p>
           <p>Privacy Policy</p>
-        </div>
+        </div> */}
       </footer>
     </div>
   )
