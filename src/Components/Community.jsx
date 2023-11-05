870fac31be172b8edc4d287385c7679c7e65ef70
mAyush24 <ayush2524mishra@gmail.com>
2023-11-05 19:52:40 +0530

    ...

--- src/Components/Community.jsx
+++ src/Components/Community.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import css from './Community.module.css'
-import { FaLinkedin, FaWhatsapp, FaQuora, FaDiscord } from "react-icons/fa";
+import { FaLinkedin, FaWhatsapp, FaQuora, FaDiscord, FaInstagram } from "react-icons/fa";
 
 const Community = () => {
     return (
@@ -11,12 +11,12 @@ const Community = () => {
                     <div className={css.links}>
                         <FaWhatsapp color="white" size="6rem" />
                         <h1 className={css.name}>WhatsApp Community</h1>
-                        <a className={css.btn} href="https://wa.me/message/37PSY2CRRSIJE1">Join Community</a>
+                        <a className={css.btn} href="https://chat.whatsapp.com/LhMiXCqeYg6Lm7hJa8nseW">Join Community</a>
                     </div>
                     <div className={css.links}>
-                        <FaLinkedin color="white" size="6rem" />
-                        <h1 className={css.name}>LinkedIn Community</h1>
-                        <a className={css.btn} href="https://www.linkedin.com/company/know-your-colleges/">Join Community</a>
+                        <FaInstagram color="white" size="6rem" />
+                        <h1 className={css.name}>InstaGram Community</h1>
+                        <a className={css.btn} href="https://instagram.com/knowyourcolleges_kyc?igshid=OGQ5ZDc2ODk2ZA==">Join Community</a>
                     </div>
                 </div>
                 <div className={css.pages}>
